1be1c9be44e892d77481799bc54e5f9a57c353df
paweljabloniec12 <146932810+paweljabloniec12@users.noreply.github.com>
2023-11-29 12:08:56 +0100

    Laboratorium 8

---
+++ lab08/lab08-engine/src/main/java/pwo/lab08/engine/Engine.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package pwo.lab08.engine;
+
+public class Engine {
+
+    public static final String
+ VERSION = "Number sequences engine version 1.0.0";
+
+ public static String getVersion() {
+ return VERSION;
+ }
+
+}
